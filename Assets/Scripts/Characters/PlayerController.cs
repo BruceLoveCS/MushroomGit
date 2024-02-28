6d606eb5c7e04545824ef593823bc23fc14b4618
BruceLoveCS <wangyudong0225@163.com>
2024-02-28 23:48:32 +0000

    ver0.1.0
    rewrite the code and bake the map

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -7,14 +7,27 @@ public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent agent;
 
+    private Animator anim;
+
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     void Start()
     {
         MouseManager.Instance.OnMouseClicked += MoveToTarget;
+    }
+
+    void Update()
+    {
+        SwitchAnimation();
+    }
+
+    private void SwitchAnimation()
+    {
+        anim.SetFloat("Speed", agent.velocity.sqrMagnitude);
     }
 
     public void MoveToTarget(Vector3 target)
